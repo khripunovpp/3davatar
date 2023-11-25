4b54480c30c53dd35a3be720e53b5ee6c95ff22d
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2023-11-25 16:39:18 +0000

    added mouse following

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import {Canvas, useThree} from "@react-three/fiber";
+import {Canvas, useFrame, useThree} from "@react-three/fiber";
 import './App.scss';
 import {MeshPortalMaterial, OrbitControls, Stats, StatsGl, useTexture} from "@react-three/drei";
 import {Me} from "./Me.jsx";
 import * as THREE from "three";
-import {useLayoutEffect} from "react";
+import {Vector3} from "three";
+import {useLayoutEffect, useRef} from "react";
 
 function App() {
   const DEV = false;
@@ -17,41 +18,56 @@ function App() {
         <gridHelper rotation-x={Math.PI / 2} position-y={0}/>
         <Stats/>
         <StatsGl/>
+        <OrbitControls enableDamping={true}
+                       maxPolarAngle={1.5}
+                       minPolarAngle={1.1}
+                       maxAzimuthAngle={.2}
+                       minAzimuthAngle={-.2}
+                       enableZoom={true}
+                       makeDefault/>
       </>}
-      <OrbitControls enableDamping={true}
-                     maxPolarAngle={1.5}
-                     minPolarAngle={1.1}
-                     maxAzimuthAngle={.2}
-                     minAzimuthAngle={-.2}
-                     enableZoom={true}
-                     makeDefault/>
-
+      <Rig/>
       <Scene/>
     </Canvas>
   )
 }
 
 function Scene() {
+  const groupRef = useRef();
   const map = useTexture('/env.jpg');
-  const camera = useThree((state) => state.camera);
+  const {camera, mouse} = useThree();
   useLayoutEffect(() => {
     camera.position.set(0, 0, .5);
   });
-  return <mesh>
-    <sphereGeometry args={[.22, 15, 15]}/>
-    <MeshPortalMaterial>
-      <directionalLight position={[0, 1, 1]} intensity={5} theatreKey={'light'}/>
-      <directionalLight position={[0, -1, 1]} intensity={5} theatreKey={'light'}/>
 
-      <mesh>
-        <sphereGeometry args={[10, 32, 32]}/>
-        <meshStandardMaterial map={map} side={THREE.BackSide}/>
-        <group position={[0, -.1, 0]}>
-          <Me/>
-        </group>
-      </mesh>
-    </MeshPortalMaterial>
-  </mesh>
+
+  return <group ref={groupRef}>
+    <mesh>
+      <sphereGeometry args={[.22, 15, 15]}/>
+      <MeshPortalMaterial>
+        <directionalLight position={[0, 1, 1]} intensity={5} theatreKey={'light'}/>
+        <directionalLight position={[0, -1, 1]} intensity={5} theatreKey={'light'}/>
+
+        <mesh>
+          <sphereGeometry args={[10, 32, 32]}/>
+          <meshStandardMaterial map={map} side={THREE.BackSide}/>
+          <group position={[0, -.1, 0]}>
+            <Me/>
+          </group>
+        </mesh>
+      </MeshPortalMaterial>
+    </mesh>
+  </group>
+}
+
+function Rig() {
+  const vec = new Vector3();
+  return useFrame(({camera, mouse}) => {
+    vec.set(-mouse.x * .2, -mouse.y * .2, .5)
+    console.log(vec)
+    camera.position.lerp(vec, 0.025)
+    camera.lookAt(0, 0, 0)
+  })
 }
 
 export default App
