96ed1bf8a154e96625bad1c06379a0c0062ed233
Pavel Khripunov <pavel.khripunov@it.adcombo.com>
2023-11-30 14:46:55 +0000

    spinner

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import {createRoot} from 'react-dom/client'
 import App from "./App.jsx";
 import {Suspense} from "react";
+import Loader from "./Spinner.jsx";
+import SpinnerComponent from "./Spinner.jsx";
 
-createRoot(document.getElementById('root')).render(<Suspense fallback={null}>
+createRoot(document.getElementById('root')).render(<Suspense fallback={<SpinnerComponent />}>
   <App/>
 </Suspense>)
